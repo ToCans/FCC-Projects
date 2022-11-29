1249c9a7f6bde84bd2d58120b6e314f8b15c324f
ToCans <liverpool8914@gmail.com>
2022-11-29 10:58:47 +0100

    React Portion of Frontend Dev

---
+++ Frontend_Development_Libraries/React/create-a-complex-jsx-element.jsx
@@ -0,0 +1,9 @@
+let JSX = <div>
+            <h1>Header Title</h1>
+            <p>Text will go here!</p>
+            <ul>
+              <li>One</li>
+              <li>Two</li>
+              <li>Three</li>
+            </ul>
+         </div>
---
+++ Frontend_Development_Libraries/React/create-a-stateless-functional-component.jsx
@@ -0,0 +1,11 @@
+const MyComponent = function() {
+  // Change code below this line
+  return(
+    <div>
+      <p>Text goes here</p>
+    </div>
+  )
+
+
+  // Change code above this line
+}
